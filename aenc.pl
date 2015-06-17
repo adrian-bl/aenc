cb0382463542600f4dc16f1cdd196a2ace18c878
Adrian Ulrich <adrian@blinkenlights.ch>
2015-06-17 19:26:04 +0200

    ffmpeg all things!

--- aenc.pl
+++ aenc.pl
@@ -1,197 +1,29 @@
 #!/usr/bin/perl
 use strict;
-use Getopt::Long;
 
-my $opts = {};
+die "Usage: $0 FILE_LIST\n" unless int(@ARGV);
 
-GetOptions($opts, "profile=s", "hardsub");
+foreach my $input_file (@ARGV) {
 
-my $subclass = Aenc::Init->($opts->{profile});
+	my($part_name) = $input_file =~ /^(.+)\.([^\.]+)$/;
+	next unless $part_name;
 
-die "Usage: $0 --profile=[legend|defy|defyhq|hq2] [--hardsub] INPUT\n" unless $subclass;
+	my $tmp_file = $part_name."_tmp.mp4";
+	my $out_file = $part_name."_encoded.mp4";
 
-foreach my $infile (@ARGV) {
-	my ($outfile) = $infile =~ /(.+)\....$/;
-	my @cmd       = ();
-	$outfile    ||= int(rand(0xFFFFF));
-	$outfile     .= "-android-$opts->{profile}.mp4";
-	print "> Encoding $infile -> $outfile\n";
-	
-	if($opts->{hardsub}) {
-		$subclass->do_hardsub($infile,$outfile);
-	}
-	else {
-		$subclass->do_mpeg4($infile,$outfile);
-	}
-	
+	my @cmdlist = ('-i',         $input_file,
+	               '-vf',        'scale=1024:576,subtitles=\''.$input_file.'\'',
+	               '-c:v',       'libx264',
+	               '-profile:v', 'baseline',
+	               '-c:a',       'libfaac',
+	               '-ar',        '44100',
+	               '-ac',        '2',
+	               '-b:a',       '128k',
+	               '-preset',    'medium',
+	               $tmp_file);
+
+	unlink($tmp_file); # do not waste time waiting for input
+
+	my $rv = system("ffmpeg-26", @cmdlist);
+	rename($tmp_file, $out_file) if !$rv;
 }
-
-package Aenc;
-	sub Init {
-		my($pn) = @_;
-		return Aenc::Legend->new   if $pn eq 'legend';
-		return Aenc::Defy->new     if $pn eq 'defy';
-		return Aenc::DefyHQ->new   if $pn eq 'defyhq';
-		return Aenc::HQ2->new      if $pn eq 'hq2';
-		return undef;
-	}
-1;
-
-package Aenc::Generic;
-	
-	###############################################
-	# Display generic info
-	sub info {
-		my($self,$msg) = @_;
-		print "[info] $msg\n";
-	}
-	
-	###############################################
-	# Critical / Error messages
-	sub critical {
-		my($self,$msg) = @_;
-		print "[crit] $msg\n";
-	}
-	
-	###############################################
-	# Run mencoder to hardsub $infile - returns name of tempfile
-	sub do_mencoder_hardsub {
-		my($self,$infile) = @_;
-		my $tmp = "tmp.$$.".int(rand(0xFFFF)).".avi";
-		$self->info("Creating tempfile ($tmp)");
-		
-		my @forced_aid = ();
-		my @forced_sid = ();
-		
-		if(exists($ENV{FORCE_SID})) {
-			push(@forced_sid, "-sid", $ENV{FORCE_SID});
-		}
-		if(exists($ENV{FORCE_AID})) {
-			push(@forced_aid, "-aid", $ENV{FORCE_AID});
-		}
-		
-		system("mencoder", "-oac", "pcm", "-ovc", "lavc", "-lavcopts", "vcodec=ffv1", @forced_sid, @forced_aid, "-o", $tmp, $infile);
-		return $tmp;
-	}
-	
-	###############################################
-	# Create a hardsub - then call mpeg4 encoder
-	sub do_hardsub {
-		my($self,$in,$out) = @_;
-		my $hsub = $self->do_mencoder_hardsub($in);
-		if(-f $hsub) {
-			$self->do_mpeg4($hsub,$out);
-			unlink($hsub) or die "Could not unlink `$hsub': $!\n";
-		}
-		else {
-			$self->critical("Failed to mencode `$in'");
-			unlink($hsub);
-		}
-	}
-	
-	###############################################
-	# Call ffmpeg to create an mpeg4 file
-	sub do_mpeg4 {
-		my($self,$in,$out) = @_;
-		$self->info("Converting `$in' into `$out'");
-		
-		if($self->do_twopass) {
-			foreach my $pass (1..2) {
-				print "# pass $pass\n";
-				my @ff = ("ffmpeg", "-y", "-pass", $pass, "-i", $in,$self->get_vcodec_args, $self->get_vcodec_extargs, $self->get_acodec_args, $out);
-				$self->info(join(" ",@ff));
-				system(@ff);
-			}
-		}
-		else {
-			my @ff = ("ffmpeg", "-i", $in,$self->get_vcodec_args, $self->get_vcodec_extargs, $self->get_acodec_args, $out);
-			$self->info(join(" ",@ff));
-			system(@ff);
-		}
-	}
-	
-	###############################################
-	# Return (generic) faac opts
-	sub get_acodec_args {
-		my($self) = @_;
-		return qw(-acodec libfaac -ab 128k -ar 44100);
-	}
-	
-	###############################################
-	# Return (generic) x264 opts
-	sub get_vcodec_args {
-		return qw(-vcodec libx264 -s 480x272 -b 480k -maxrate 1024k -bufsize 2M);
-	}
-	
-	###############################################
-	# even more x264 opts!
-	sub get_vcodec_extargs {
-		return qw(-profile baseline -threads 0);
-	}
-	
-	sub do_twopass {
-		return 0;
-	}
-	
-1;
-
-package Aenc::Legend;
-	use base 'Aenc::Generic';
-	
-	sub new {
-		my($classname,%args) = @_;
-		my $self = {};
-		return bless($self,$classname);
-	}
-	
-1;
-
-package Aenc::DefyHQ;
-	use base 'Aenc::Generic';
-	sub new {
-		my($classname,%args) = @_;
-		my $self = {};
-		return bless($self,$classname);
-	}
-	###############################################
-	# Set resolution to native-defyness
-	sub get_vcodec_args {
-		return qw(-vcodec libx264 -s 854x480 -b 950k -maxrate 1900k -bufsize 2M);
-	}
-1;
-
-package Aenc::Defy;
-	use base 'Aenc::Generic';
-	sub new {
-		my($classname,%args) = @_;
-		my $self = {};
-		return bless($self,$classname);
-	}
-	###############################################
-	# Set resolution to native-defyness
-	sub get_vcodec_args {
-		return qw(-vcodec libx264 -s 640x360 -b 400k -maxrate 800k -bufsize 2M);
-	}
-1;
-
-
-package Aenc::HQ2;
-	use base 'Aenc::Generic';
-	sub new {
-		my($classname,%args) = @_;
-		my $self = {};
-		return bless($self,$classname);
-	}
-	
-	sub do_twopass {
-		return 1;
-	}
-	
-	###############################################
-	# Set resolution to native-defyness
-	sub get_vcodec_args {
-		return qw(-vcodec libx264 -s 854x480 -b 950k -maxrate 1900k -bufsize 2M);
-	}
-	
-1;
-
